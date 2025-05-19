039ee5057da30cb89f8d807f0783e87bfe4ac8c3
mounika kolliboyina <183710046+mounikakolliboyina@users.noreply.github.com>
2025-05-19 14:25:48 +0530

    -m

--- src/probablity/Probability.java
+++ src/probablity/Probability.java
@@ -1,10 +1,12 @@
 package probablity;
 
 public class Probability {
+
     public static double probablityOf(double totalOutComes, double favourableOutComes) {
         if (totalOutComes < favourableOutComes){
             throw new RuntimeException("invalid probability");
         }
+
         return  favourableOutComes / totalOutComes;
     }
 }
--- test/probablity/ProbabilityTest.java
+++ test/probablity/ProbabilityTest.java
@@ -8,11 +8,18 @@ class ProbabilityTest {
 
     @Test
     void tailProbability() {
-        assertEquals(0.5, Probability.probablityOf(2,1));
+
+        assertEquals(0.5, Probability.probablityOf(2, 1));
     }
 
     @Test
     void nonTailProbability() {
-        assertEquals(0.5,1 - Probability.probablityOf(2,1));
+        assertEquals(0.5, 1 - Probability.probablityOf(2, 1));
     }
+
+    @Test
+    void twoTailsProbability() {
+        assertEquals(0.25,Probability.probablityOf(4,1));
+    }
+
 }
